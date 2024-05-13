e74dfadceaf56dad2f4827142468bfcba64164f0
Bernhard Sigl <b-sigl@live.de>
2024-05-13 20:07:05 +0200

    add login

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,13 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { AuthInterceptorService } from './services/auth-interceptor.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([AuthInterceptorService]), withFetch())
+  ]
 };
---
+++ src/app/authentication/login/login.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+export class LoginComponent {
+  username: string = '';
+  password: string = '';
+
+  constructor(private as: AuthService, private router: Router) {}
+
+  async login() {
+    try {
+      let resp: any = await this.as.loginWithUsernameAndPassword(
+        this.username,
+        this.password
+      );
+      localStorage.setItem('token', resp['token']);
+      localStorage.setItem('author', resp['user_id']);
+      this.router.navigateByUrl('/todos');
+      console.log('resp: ', resp);
+    } catch (e) {
+      alert('Login fehlgeschlagen')
+      console.error('Error: ', e);
+    }
+  }
+}
